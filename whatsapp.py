3e37e3817a36c7ebd99ef6e316c91bdec9d487f1
thomasqueirozb <38295417+thomasqueirozb@users.noreply.github.com>
2018-11-09 15:02:56 -0200

    Inicio das fotos

--- whatsapp.py
+++ whatsapp.py
@@ -1,11 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
-from os import system
+import os, sys
 import code
 
+
+
+
+options = webdriver.chrome.options.Options()
+options.add_argument("--disable-extensions")
+
 # Abrir o Chrome driver
-b = webdriver.Chrome("./chromedriver.exe")
+b = webdriver.Chrome("./chromedriver.exe", chrome_options = options)
 
 # Entrar no Whatsapp Web
 b.get('http://web.whatsapp.com')
@@ -20,18 +26,21 @@ def REPL():
 
 
 
-def clear():
-	# Limpar o console
-	_ = system('cls')
+def cls():
+	# Limpa o console
+    _ = os.system('cls' if os.name=='nt' else 'clear')
 
 
 while True:
-
+	# Pede o nome da pessoa
 	nome = input("Digite o nome do grupo/pessoa: ").lower()
 
+	# Se o nome da pessoa for exec ou repl, entrar em debug mode
 	if nome == 'exec' or nome == 'repl':
 		REPL()
 		continue
+
+	# Se o nome da pessoa for exit, para o programa
 	elif nome == "exit":
 		break
 
@@ -58,9 +67,11 @@ while True:
 
 		elif len(possiveis_nomes) > 1:
 			try:
-				for i,j in zip(possiveis_nomes,range(len(possiveis_nomes))):
+				for j,i in enumerate(possiveis_nomes):
 					print(f'{j+1} - {i[0].text}')
-				name_index = int(input("Digite o número da pessoa/grupo: ")) - 1
+				name_index = input("Digite o número da pessoa/grupo: ")
+				name_index = int(name_index) - 1
+				
 				elem[name_index][1].click()
 				print(f'{elem[name_index][0].text} foi selecionado')
 				cond = True
@@ -110,61 +121,81 @@ while True:
 
 
 
-	msg = input("Digite a mensagem: ")
+	msg = input("Digite a mensagem ou 'foto': ")
 
-	if msg == "exit":
+
+
+	if msg == 'exit':
 		break
 	elif msg == 'change':
 		continue
 
+	if msg != 'foto':
+		modo = input("0- Várias mensagens\n1- Letra por letra\n> ")
 
-	modo = input("0- Várias mensagens\n1- Letra por letra\n> ")
+		try:
+			txtBox = b.find_elements_by_class_name('_2S1VP')[0] # Caixa de texto
+			if modo == "0":
+				try:
+					nvezes=int(input("Digite o número de vezes: "))
+					cond = True
+					txtBox.clear()
 
+					if '@' not in msg:
+						txtBox.send_keys(msg)
+					else:
+						msg = msg.split('@')
+						firstPart = msg.pop(0)
+						txtBox.send_keys(firstPart)
+						for part in msg:
+							part = part.split()
+							txtBox.send_keys('@' + part.pop(0))
+							txtBox.send_keys(Keys.TAB)
+							part = ' '.join(part)
+							txtBox.send_keys(part + " ")
 
+					# Seleciona e copia
+					txtBox.send_keys(Keys.CONTROL, 'a')
+					txtBox.send_keys(Keys.CONTROL, 'c')
+					for i in range(nvezes):
+						# Cola
+						txtBox.send_keys(Keys.CONTROL, 'v')
+						txtBox.send_keys(Keys.ENTER)
 
-	try:
-		txtBox = b.find_elements_by_class_name('_2S1VP')[0] # Caixa de texto
-		if modo == "0":
+				except KeyboardInterrupt:
+					pass
+
+			elif modo == "1":
+				msg = msg.replace(" ","")
+				for i in msg:
+					txtBox.send_keys(i)
+					txtBox.send_keys(Keys.ENTER)
+
+			clear()
+
+		except:
+			print("Caixa de texto não encontrada")
+	else:
+		print('Foto')
+
+		try:
+			txtBox = b.find_elements_by_class_name('_2S1VP')[0] # Caixa de texto
 			try:
 				nvezes=int(input("Digite o número de vezes: "))
 				cond = True
+				path=input("Path da photo:")
 
-				if '@' not in msg:
-					txtBox.clear()
-					txtBox.send_keys(msg)
-				else:
-					txtBox.clear()
-					msg = msg.split('@')
-					firstPart = msg.pop(0)
-					txtBox.send_keys(firstPart)
-					for part in msg:
-						part = part.split()
-						txtBox.send_keys('@' + part.pop(0))
-						txtBox.send_keys(Keys.TAB)
-						part = ' '.join(part)
-						txtBox.send_keys(part + " ")
-
-				# Seleciona e copia
-				txtBox.send_keys(Keys.CONTROL, 'a')
-				txtBox.send_keys(Keys.CONTROL, 'c')
+				# copiar imagem pro clipboard
 				for i in range(nvezes):
 					# Cola
 					txtBox.send_keys(Keys.CONTROL, 'v')
 					txtBox.send_keys(Keys.ENTER)
-
-			except KeyboardInterrupt:
+			except:
 				pass
+		except KeyboardInterrupt:
+			pass
 
-		elif modo == "1":
-			msg = msg.replace(" ","")
-			for i in msg:
-				txtBox.send_keys(i)
-				txtBox.send_keys(Keys.ENTER)
 
-		clear()
-
-	except:
-		print("Caixa de texto não encontrada")
-
-from sys import exit
-exit()
+b.close()
+b.quit()
+sys.exit()
